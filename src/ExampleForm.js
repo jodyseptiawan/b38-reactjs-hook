f5398772d7c716c46e6722e0c00250db98081737
Jody-septiawan <jody.septiawan5@gmail.com>
2022-09-02 08:44:31 +0700

    State Class Comp

--- src/ExampleForm.js
+++ src/ExampleForm.js
@@ -1,67 +1,86 @@
-import React, { Component } from 'react';
-import { 
-        Container, 
-        Row, Col, Form, 
-        Button } from 'react-bootstrap'
+import React, { Component } from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 
 class ExampleForm extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     // init state attribute here
+    this.state = {
+      fullname: "",
+      email: "",
+      password: "",
+    };
   }
 
   handleOnChange = (e) => {
     // setState here
-  }
+    const data = e.target.value;
+    const name = e.target.name;
+    console.log(name);
+
+    this.setState({
+      ...this.state,
+      [name]: data,
+    });
+  };
 
   handleOnSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     //print state value with console.log here
-  }
+    console.log(this.state);
+  };
 
-  render(){
+  render() {
     return (
       <Container>
-        <Row 
-          className="d-flex align-items-center justify-content-center vh-100">
+        <Row className="d-flex align-items-center justify-content-center vh-100">
           <Col md="6">
-          <Form onSubmit={this.handleOnSubmit}>
-            <div className="text-center h3">Register</div>
-            <Form.Group className="mb-3" controlId="formFullName">
-              <Form.Label>Full Name</Form.Label>
-              <Form.Control 
-                onChange={this.handleOnChange} 
-                value={this.state.fullname}
-                name="fullname" size="sm" type="text" 
-                placeholder="Enter Full Name" />
-            </Form.Group>
+            <Form onSubmit={this.handleOnSubmit}>
+              <div className="text-center h3">Register</div>
+              <Form.Group className="mb-3" controlId="formFullName">
+                <Form.Label>Full Name</Form.Label>
+                <Form.Control
+                  onChange={this.handleOnChange}
+                  value={this.state.fullname}
+                  name="fullname"
+                  size="sm"
+                  type="text"
+                  placeholder="Enter Full Name"
+                />
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control 
-                onChange={this.handleOnChange} 
-                value={this.state.email}
-                name="email" size="sm" type="email" 
-                placeholder="Enter email" />
-            </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  onChange={this.handleOnChange}
+                  value={this.state.email}
+                  name="email"
+                  size="sm"
+                  type="email"
+                  placeholder="Enter email"
+                />
+              </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control 
-                onChange={this.handleOnChange} 
-                value={this.state.password}
-                name="password" size="sm" type="password" 
-                placeholder="Password" />
-            </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  onChange={this.handleOnChange}
+                  value={this.state.password}
+                  name="password"
+                  size="sm"
+                  type="password"
+                  placeholder="Password"
+                />
+              </Form.Group>
 
-            <Button variant="primary" type="submit" size="sm">
-              Submit
-            </Button>
-          </Form>
+              <Button variant="primary" type="submit" size="sm">
+                Submit
+              </Button>
+            </Form>
           </Col>
         </Row>
       </Container>
-    )
+    );
   }
 }
 
